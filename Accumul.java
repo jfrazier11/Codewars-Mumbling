9cce1882b92a92653e49215fc2b1be6c20eb8447
Jeremi Frazier <jerm_803@yahoo.com>
2016-05-13 15:13:18 -0400

    Update Accumul.java

--- Accumul.java
+++ Accumul.java
@@ -1,6 +1,16 @@
 import java.lang.StringBuilder;
+
+/**
+ * Transforms text specified by description
+ * @author Jeremi R Frazier
+ * @Version 1.0
+ **/
 public class Accumul {
-    
+    /**
+     * a method that takes a word and extends it by its length turning the first character into a lowercase
+     * @param s - The word
+     * @return String - the accumul
+     */ 
     public static String accum(String s) {
     StringBuilder strb = new StringBuilder("");
      for(int i=0; i < s.length();i++)
